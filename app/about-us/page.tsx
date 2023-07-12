9767b55c3533d9c67a768d2fa59781fb77e35197
Ben Haig <bdhaig@gmail.com>
2023-07-12 11:53:02 -0400

    Actual Pre-lesson commit

---
+++ app/about-us/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutUs() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1 className='text-center text-3xl font-bold'>About Us - Nextjs Sitemap Tutorial</h1>
+        </main>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className='text-center text-3xl font-bold'>Homepage - Nextjs Sitemap Tutorial</h1>
+    <main className="p-8">
+      <h1 className='text-center text-3xl font-bold mb-8'>Homepage - Nextjs Sitemap Tutorial</h1>
+      <p className="text-lg text-center">Learn how to create a static or dynamic sitemap for your next js application!</p>
+      <Image src={'/next-sitemap-thumbnail.png'} alt="Next Js Sitemap Tutorial!" width={480} height={270} className="mx-auto rounded" />
+      <p className="text-center">By: Ben Haig, please subscribe if you find the content useful!</p>
     </main>
   )
 }
